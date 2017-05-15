f0f41875351cc3e6ff8c5533a4f8c139ef425b33
BuildTools <unconfigured@null.spigotmc.org>
2017-05-15 09:59:36 +0200

    Project Frei - Commit Sander
    First Commit

---
+++ Assets/MetroScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetroScript : MonoBehaviour {
+
+	public Transform[] target;
+	public float speed;
+	private int current;
+
+	public float Damping = 100.0f;
+
+	public bool playerIsNear = false;
+
+	public bool nextStation;
+
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.E))
+	{
+		nextStation = true;
+	}
+
+		if (playerIsNear == true && nextStation == true){
+
+				if(transform.position != target[current].position)
+				{
+					
+					Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+					var rotation = Quaternion.LookRotation (target[current].position - transform.position);
+					transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * Damping);
+					GetComponent<Rigidbody>().MovePosition(pos);
+					speed = 5;
+				}
+
+				else {
+					current = (current + 1) % target.Length;
+					nextStation = false;
+					
+				}
+			}
+		}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+				playerIsNear = true;	
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			playerIsNear = false;
+		}
+	}
+
+	void OnGui()
+	{
+		if (playerIsNear == true)
+		{
+			 GUI.Label(new Rect(Screen.width /2, Screen.height / 2, 100, 20), "Hello World!");
+		}
+	}
+}
