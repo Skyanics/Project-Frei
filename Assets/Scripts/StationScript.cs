f3a022fac7d3cba1eb7ee7706df1db055194bd28
Sander <sanderwinkeler2@gmail.com>
2017-05-30 16:26:44 +0200

    **UPDATE**
    - Metro script verbeterd stopt nu bij stations :)

---
+++ Assets/Scripts/StationScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			GameObject.Find("Metro").GetComponent<MetroScript>().nextStation = false;
+		}
+	}
+}
