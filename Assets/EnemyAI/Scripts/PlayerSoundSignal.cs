d339c7c17e3639d379bf6e18b75b93dcc5a89419
shikariix <sarahsteenhuis@live.nl>
2017-06-06 20:45:00 +0200

    **UPDATE 7**
    - Monster follows the sounds the player makes
    - Monster checks out door if he hears it open

---
+++ Assets/EnemyAI/Scripts/PlayerSoundSignal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundSignal : MonoBehaviour {
+
+	public GameObject enemy;
+	HearingScript enemyHearing;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		enemyHearing = enemy.GetComponent<HearingScript> ();
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void Update() {
+		if (audio.isPlaying) {
+			enemyHearing.HearNoise (gameObject);
+		}
+	}
+}
--- Assets/EnemyAI/Scripts/UnlockDoor.cs
+++ Assets/EnemyAI/Scripts/UnlockDoor.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class UnlockDoor : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject enemy;
 	PickUpKey pickupKey;
 	public Rigidbody rb;
+
+	HearingScript enemyHearing;
 
 
 	// Use this for initialization
 	void Start () {
 		pickupKey = player.GetComponent<PickUpKey>();
+		enemyHearing = enemy.GetComponent<HearingScript> ();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +23,9 @@ public class UnlockDoor : MonoBehaviour {
 		if (Input.GetButton("Fire1") && pickupKey.hasKey == true) {
 			rb.isKinematic = false;
 			pickupKey.hasKey = false;
+			enemyHearing.HearNoise (gameObject);
+			//play unlock sound
 		}
 	}
+
 }
