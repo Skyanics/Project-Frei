9272d2c38ebd92aec873f3553e2eeda4469839c5
Skyanics (HomePC) <unconfigured@null.spigotmc.org>
2017-06-08 10:07:33 +0200

    **BUILD ALPHA**
    Test

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void StartGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
