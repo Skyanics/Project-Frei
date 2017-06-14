57f35846a5d25ca3b6cae0bd4913917ca32879a7
Skyanics (HomePC) <unconfigured@null.spigotmc.org>
2017-06-14 19:55:58 +0200

    **UPDATE 13**
    - Added random sound for monster

---
+++ Assets/SanScripts/MonsterRandomSoundScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterRandomSoundScript : MonoBehaviour {
+
+	public int randomizer;
+	public AudioSource aS;
+	private IEnumerator coroutine;
+
+	public int minVal = 10;
+	public int maxVal = 40;
+	
+	void Start()
+	{
+		coroutine = RandomPlaySound();
+		StartCoroutine(coroutine);
+	}
+
+	private IEnumerator RandomPlaySound()
+	{
+		while(true){
+			yield return new WaitForSeconds(randomizer);
+			aS.Play();
+			randomizer = Random.Range(minVal, maxVal);
+		}
+	}
+}
